b83054d1ab2609033d53b0899fb7f9937bc2060b
Kugelbltz <arifkalndr@gmail.com>
2025-04-05 17:22:13 +0300

    Initial commit

---
+++ src/main/java/me/Kugelbltz/jadePack/JadePack.java
@@ -0,0 +1,16 @@
+package me.Kugelbltz.jadePack;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JadePack extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+
+
+}
