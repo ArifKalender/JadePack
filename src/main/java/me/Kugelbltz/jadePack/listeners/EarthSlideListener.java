53fb46159bf98d85241103f3eb57abba84ba13c7
Kugelbltz <arifkalndr@gmail.com>
2025-04-10 23:07:36 +0300

    0.0.8.1

--- src/main/java/me/Kugelbltz/jadePack/listeners/EarthSlideListener.java
+++ src/main/java/me/Kugelbltz/jadePack/listeners/EarthSlideListener.java
@@ -5,6 +5,7 @@ import com.projectkorra.projectkorra.ability.CoreAbility;
 import com.projectkorra.projectkorra.ability.EarthAbility;
 import com.projectkorra.projectkorra.earthbending.EarthArmor;
 import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,28 +18,55 @@ import static me.Kugelbltz.jadePack.JadePack.plugin;
 
 public class EarthSlideListener implements Listener {
 
-    float speed = (float) plugin.getConfig().getDouble("Abilities.EarthSlide.Speed");
 
     @EventHandler
     private void onSneak(PlayerToggleSneakEvent event){
         if(event.isSneaking()){
             Player player = event.getPlayer();
             if(CoreAbility.hasAbility(player, EarthArmor.class)){
+                float speed = (float) plugin.getConfig().getDouble("Abilities.EarthSlide.Speed");
+                double step_height = player.getAttribute(Attribute.STEP_HEIGHT).getBaseValue();
                 new BukkitRunnable(){
                     @Override
                     public void run() {
-                        if(!player.isSneaking() || !CoreAbility.hasAbility(player, EarthArmor.class)){
+                        player.getAttribute(Attribute.STEP_HEIGHT).setBaseValue(1);
+
+
+                        if(!player.isOnline() || player.isDead()){
+                            player.getAttribute(Attribute.STEP_HEIGHT).setBaseValue(step_height);
                             this.cancel();
                         }
-                        Location location = new Location(player.getWorld(),player.getLocation().getX(), player.getLocation().getY()-0.5, player.getLocation().getZ());
-                        Block block = location.getBlock();
-                        if(EarthAbility.isEarthbendable(player,block)){
+                        if(!player.isSneaking() || !CoreAbility.hasAbility(player, EarthArmor.class) ){
+                            player.getAttribute(Attribute.STEP_HEIGHT).setBaseValue(step_height);
+                            this.cancel();
+                        }
+
+                        if(returnBendable(player) && BendingPlayer.getBendingPlayer(player).getBoundAbilityName().equalsIgnoreCase("EarthArmor")){
                             Vector vector = player.getEyeLocation().getDirection();
-                            player.setVelocity(vector.multiply(speed));
+                            vector.setY(-1);
+                            player.setVelocity(vector.normalize().multiply(speed));
                         }
                     }
                 }.runTaskTimer(plugin,0,1);
             }
         }
+    }
+
+
+
+    private boolean returnBendable(Player player){
+        Block below;
+        Block behind;
+        Block front;
+
+        below = new Location(player.getWorld(),player.getLocation().getX(),player.getLocation().getY()-0.1,player.getLocation().getZ()).getBlock();
+        behind = below.getLocation().add(player.getLocation().getDirection().setY(0).normalize().multiply(-1)).getBlock();
+        front = below.getLocation().add(player.getLocation().getDirection().setY(0).normalize()).getBlock();
+
+        if(EarthAbility.isEarthbendable(player,below) || EarthAbility.isEarthbendable(player,front) || EarthAbility.isEarthbendable(player,behind)){
+            return true;
+        }
+
+        return false;
     }
 }
